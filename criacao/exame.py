1fe08a3770232d66d968515284d65a76341595d0
Luan Vasconcelos <luan@acone.com.br>
2022-03-16 11:58:19 -0300

    Padrão Builder

--- criacao/exame.py
+++ criacao/exame.py
@@ -3,95 +3,95 @@
 # Adaptar código abaixo
 
 # Abstract course
-class Course:
+class Exame:
 
     def __init__(self):
-        self.Fee()
-        self.available_batches()
+        self.Preco()
+        self.vagas_disponiveis()
 
-    def Fee(self):
+    def Preco(self):
         raise NotImplementedError
 
-    def available_batches(self):
+    def vagas_disponiveis(self):
         raise NotImplementedError
 
     def __repr__(self):
-        return 'Fee : {0.fee} | Batches Available : {0.batches}'.format(self)
+        return 'Preco : {0.preco} | Vagas disponiveis : {0.vagas}'.format(self)
 
 
-# concrete course
-class DSA(Course):
-    """Class for Data Structures and Algorithms"""
+# concrete exames
+class EDS(Exame):
+    """Class for Exame de Sangue """
 
-    def Fee(self):
-        self.fee = 8000
+    def Preco(self):
+        self.preco = 8000
 
-    def available_batches(self):
-        self.batches = 5
-
-    def __str__(self):
-        return "DSA"
-
-
-# concrete course
-class SDE(Course):
-    """Class for Software Development Engineer"""
-
-    def Fee(self):
-        self.fee = 10000
-
-    def available_batches(self):
-        self.batches = 4
+    def vagas_disponiveis(self):
+        self.vagas = 5
 
     def __str__(self):
-        return "SDE"
+        return "EDS"
 
 
-# concrete course
-class STL(Course):
-    """Class for Standard Template Library"""
+# concrete exame
+class EDV(Exame):
+    """Class for Exame de Vista"""
 
-    def Fee(self):
-        self.fee = 5000
+    def Preco(self):
+        self.preco = 10000
 
-    def available_batches(self):
-        self.batches = 7
+    def vagas_disponiveis(self):
+        self.vagas = 4
 
     def __str__(self):
-        return "STL"
+        return "EDV"
 
 
-# Complex Course
-class ComplexCourse:
+# concrete exame
+class EDC(Exame):
+    """Class for Exame de Coração"""
+
+    def Preco(self):
+        self.preco = 5000
+
+    def vagas_disponiveis(self):
+        self.vagas = 7
+
+    def __str__(self):
+        return "EDC"
+
+
+# Complex Exame
+class ComplexExame:
 
     def __repr__(self):
-        return 'Fee : {0.fee} | available_batches: {0.batches}'.format(self)
+        return 'Preco : {0.preco} | vagas_disponiveis: {0.vagas}'.format(self)
 
 
 # Complex course
-class Complexcourse(ComplexCourse):
+class Complexexame(ComplexExame):
 
-    def Fee(self):
-        self.fee = 7000
+    def Preco(self):
+        self.preco = 7000
 
-    def available_batches(self):
-        self.batches = 6
+    def vagas_disponiveis(self):
+        self.vagas = 6
 
 
-# construct course
-def construct_course(cls):
-    course = cls()
-    course.Fee()
-    course.available_batches()
+# construct exame
+def construct_exame(cls):
+    exame = cls()
+    exame.Preco()
+    exame.vagas_disponiveis()
 
-    return course  # return the course object
+    return exame  # return the exame object
 
 
 # main method
 if __name__ == "__main__":
-    dsa = DSA()  # object for DSA course
-    sde = SDE()  # object for SDE course
-    stl = STL()  # object for STL course
+    eds = EDS()  # object for EDS Exame
+    edv = EDV()  # object for EDV Exame
+    edc = EDC()  # object for EDC Exame
 
-    complex_course = construct_course(Complexcourse)
-    print(complex_course)
+    complex_exame = construct_exame(Complexexame)
+    print(complex_exame)
